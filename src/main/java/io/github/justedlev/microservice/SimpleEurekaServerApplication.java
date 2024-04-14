b1034e5763c400d64ed22e2715195f444e7a4d55
justedlev <edyardlyk@gmail.com>
2024-04-14 21:59:26 +0300

    Update

--- src/main/java/io/github/justedlev/microservice/SimpleEurekaServerApplication.java
+++ src/main/java/io/github/justedlev/microservice/SimpleEurekaServerApplication.java
@@ -2,15 +2,14 @@ package io.github.justedlev.microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class EurekaServerApplication {
+public class SimpleEurekaServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaServerApplication.class, args);
+		SpringApplication.run(SimpleEurekaServerApplication.class, args);
 	}
 
 }
---
+++ src/main/java/io/github/justedlev/microservice/configuration/SecurityConfiguration.java
@@ -0,0 +1,37 @@
+package io.github.justedlev.microservice.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.CompositeLogoutHandler;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults())
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .addLogoutHandler(new SecurityContextLogoutHandler())
+                )
+                .authorizeHttpRequests(exchangeSpec -> exchangeSpec
+                        .requestMatchers(
+                                "/error/*",
+                                "/error",
+                                "/logout"
+                        )
+                        .permitAll()
+                        .anyRequest().authenticated()
+                )
+                .build();
+    }
+}
