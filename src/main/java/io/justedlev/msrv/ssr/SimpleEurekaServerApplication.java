9774bab3ddba18b18424d11ff671ae398676929e
Justedlev <edyardlyk@gmail.com>
2024-09-24 21:07:23 +0300

    refactor: rename package name

--- src/main/java/io/justedlev/msrv/ssr/SimpleEurekaServerApplication.java
+++ src/main/java/io/justedlev/msrv/ssr/SimpleEurekaServerApplication.java
@@ -1,4 +1,4 @@
-package dev.justedlev;
+package io.justedlev.msrv.ssr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/io/justedlev/msrv/ssr/SimpleEurekaServerApplicationTests.java
+++ src/test/java/io/justedlev/msrv/ssr/SimpleEurekaServerApplicationTests.java
@@ -1,4 +1,4 @@
-package dev.justedlev.eurekaserver;
+package io.justedlev.msrv.ssr;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
