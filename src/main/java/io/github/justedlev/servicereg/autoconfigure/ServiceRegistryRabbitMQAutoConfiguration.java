4e47651f524d6cbb569144cbd61aea7932870d3b
Edward L. <edward.lukashevits@gmail.com>
2026-03-25 23:22:51 +0200

    feat: add ServiceRegistryRabbitMQAutoConfiguration.java
    Signed-off-by: Edward L. <edward.lukashevits@gmail.com>

---
+++ src/main/java/io/github/justedlev/servicereg/autoconfigure/ServiceRegistryRabbitMQAutoConfiguration.java
@@ -0,0 +1,18 @@
+package io.github.justedlev.servicereg.autoconfigure;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.amqp.autoconfigure.RabbitAutoConfiguration;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBooleanProperty;
+import org.springframework.context.annotation.Import;
+
+@AutoConfiguration
+@ConditionalOnBooleanProperty(prefix = "spring.rabbitmq", name = "enabled", matchIfMissing = true)
+@Import(RabbitAutoConfiguration.class)
+public class ServiceRegistryRabbitMQAutoConfiguration {
+    @Getter
+    @Setter(onMethod_ = @Value("${spring.rabbitmq.enabled:true}"))
+    private boolean enabled;
+}
